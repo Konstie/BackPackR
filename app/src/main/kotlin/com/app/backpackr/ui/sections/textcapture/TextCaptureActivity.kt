7b37c0e59e651db9dbe5674b605c2ed2a1a4f012
Konstie <konstieone@gmail.com>
2016-11-27 16:31:14 +0200

    Added text by blocks recognition support.

--- app/src/main/kotlin/com/app/backpackr/ui/sections/textcapture/TextCaptureActivity.kt
+++ app/src/main/kotlin/com/app/backpackr/ui/sections/textcapture/TextCaptureActivity.kt
@@ -5,6 +5,9 @@ import android.content.IntentFilter
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.ScaleGestureDetector
 import com.app.backpackr.presenters.abs.PresenterFactory
 import com.app.backpackr.presenters.textcapture.ITextCaptureView
 import com.app.backpackr.presenters.textcapture.TextCapturePresenter
@@ -12,7 +15,9 @@ import com.app.backpackr.textprocessor.OcrDetectorProcessor
 import com.app.backpackr.ui.sections.abs.BaseActivity
 import com.app.backpackr.ui.views.GraphicOverlay
 import com.app.backpackr.ui.views.OcrGraphic
+import com.google.android.gms.common.api.CommonStatusCodes
 import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.text.TextBlock
 import com.google.android.gms.vision.text.TextRecognizer
 
 /**
@@ -20,16 +25,25 @@ import com.google.android.gms.vision.text.TextRecognizer
  */
 
 class TextCaptureActivity(override val presenterFactory: PresenterFactory<TextCapturePresenter>) : BaseActivity<TextCapturePresenter, ITextCaptureView>() {
+    val TEXT_BLOCK_OBJECT = "TEXT_BLOCK_OBJECT"
+
     var presenter: TextCapturePresenter? = null
     var cameraSource: CameraSource? = null
+    var gestureDetector: GestureDetector? = null
+    var scaleGestureDetector: ScaleGestureDetector? = null
     lateinit var graphicOverlay: GraphicOverlay<OcrGraphic>
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
 
+        // todo: if permission granted
+        createCameraSource()
+
+        gestureDetector = GestureDetector(this, captureGestureListener)
+        scaleGestureDetector = ScaleGestureDetector(this, scaleListener)
     }
 
-    fun createCameraSource(autoFocus: Boolean, useFlash: Boolean) {
+    fun createCameraSource() {
         val context = applicationContext
         val textRecognizer = TextRecognizer.Builder(context).build()
         textRecognizer.setProcessor(OcrDetectorProcessor(graphicOverlay))
@@ -38,7 +52,7 @@ class TextCaptureActivity(override val presenterFactory: PresenterFactory<TextCa
             Log.w(tag(), "Camera detector dependencies are not available")
 
             val lowStorageFilter = IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW)
-            var hasLowStorage = registerReceiver(null, lowStorageFilter) != null
+            val hasLowStorage = registerReceiver(null, lowStorageFilter) != null
 
             if (hasLowStorage) {
                 Log.w(tag(), "Yout device has low storage")
@@ -49,6 +63,37 @@ class TextCaptureActivity(override val presenterFactory: PresenterFactory<TextCa
                     .setRequestedPreviewSize(1280, 1024)
                     .setRequestedFps(15.0f)
                     .build()
+        }
+    }
+
+    private fun onTap(rawX: Float, rawY: Float): Boolean {
+        val ocrGraphic = graphicOverlay.getGraphicAtLocation(rawX, rawY)
+        val text: TextBlock? = ocrGraphic?.textBlock?: return false
+        val intent = Intent()
+        intent.putExtra(TEXT_BLOCK_OBJECT, text?.value)
+        setResult(CommonStatusCodes.SUCCESS, intent)
+        finish()
+        return text != null
+    }
+
+    private val captureGestureListener = object : GestureDetector.SimpleOnGestureListener() {
+        override fun onSingleTapConfirmed(e: MotionEvent?): Boolean {
+            if (e == null) return false
+            return onTap(e.rawX, e.rawY) || super.onSingleTapConfirmed(e)
+        }
+    }
+
+    private val scaleListener = object : ScaleGestureDetector.OnScaleGestureListener {
+        override fun onScaleBegin(p0: ScaleGestureDetector?): Boolean {
+            return true
+        }
+
+        override fun onScaleEnd(p0: ScaleGestureDetector?) {
+            // todo: do zoom
+        }
+
+        override fun onScale(p0: ScaleGestureDetector?): Boolean {
+            return false
         }
     }
 
--- app/src/main/kotlin/com/app/backpackr/ui/views/OcrGraphic.kt
+++ app/src/main/kotlin/com/app/backpackr/ui/views/OcrGraphic.kt
@@ -3,6 +3,8 @@ package com.app.backpackr.ui.views
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.RectF
+import com.google.android.gms.vision.text.Text
 import com.google.android.gms.vision.text.TextBlock
 
 /**
@@ -31,12 +33,28 @@ class OcrGraphic(var overlay: GraphicOverlay<OcrGraphic>, var textBlock: TextBlo
     }
 
     override fun draw(canvas: Canvas) {
+        val rect = getProcessedRect()
+        canvas.drawRect(rect, rectPaint)
 
+        val textComponents = textBlock?.components ?: return
+        for (currentString in textComponents) {
+            val left = translateX(currentString.boundingBox.left.toFloat())
+            val bottom = translateY(currentString.boundingBox.bottom.toFloat())
+            canvas.drawText(currentString.value, left, bottom, textPaint)
+        }
     }
 
     override fun contains(x: Float, y: Float): Boolean {
-        return false
+        val rect = getProcessedRect()
+        return (rect.left < x && rect.right > x && rect.top < y && rect.bottom > y)
     }
 
-
+    fun getProcessedRect() : RectF {
+        val rect = RectF(textBlock?.boundingBox)
+        rect.left = translateX(rect.left)
+        rect.top = translateY(rect.top)
+        rect.right = translateX(rect.right)
+        rect.bottom = translateY(rect.bottom)
+        return rect
+    }
 }
